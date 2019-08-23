c43a9d1fdae33b65263c487aebc3adc2b7112e1e
조현학 <zhh102000@gmail.com>
2019-08-23 17:08:13 +0900

    loop()의 변수를 전역변수로 이동 데이터 측정값을 2번 write를 해서 bluetooth 송신값이 끊어지는 경우가 발생했다고 생각해서 2개의 측정값을 하나의 문자열로 합쳐서 송신

--- air_quality_sensor/air_quality_sensor.ino
+++ air_quality_sensor/air_quality_sensor.ino
@@ -1,15 +1,19 @@
 #include <Wire.h>
 #include <HDC1080.h>
 #include <SoftwareSerial.h>
+#include <string.h>
 
 HDC1080 hdcSensor;
 
 float tc, tf, h;
-const int temperaturePin = 0;
-const int TX = 2;
-const int RX = 3;
-SoftwareSerial BLE(TX,RX);
+const int TX = 2, RX = 3;
+int humid, Temperate;
 
+char str_humid[10];
+char str_Temperate[10];
+char *SEND;
+
+SoftwareSerial BLE(TX,RX);
 
 void setup() {
   Serial.begin(9600);
@@ -26,16 +30,10 @@ void setup() {
 
 void loop() {
 
-  int humid;
-  int Temperate;
-  
-  char str_humid[10];
-  char str_Temperate[10];
   /*
    * 현재는 정수형을 입력하지만 실수형으로 변경
    */
 
-  
   tf = hdcSensor.getTemperatureHumidity(tc, h);
   Temperate = (int)tc;
   humid = (int)h;
@@ -45,14 +43,12 @@ void loop() {
   Serial.print(h);
   Serial.println("H");
   
-  
   sprintf(str_Temperate,"%d",Temperate); //센서값 char*형변환
   sprintf(str_humid,"%d",humid); //센서값 char*형변환
+  SEND = strcat(str_Temperate,str_humid);
   
-
   BLE.write(" ");
-  BLE.write(str_Temperate);
-  BLE.write(str_humid);
+  BLE.write(SEND);
 
   delay(1000); 
 }
