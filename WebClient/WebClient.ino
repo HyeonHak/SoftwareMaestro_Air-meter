25751708de419b65ee2f5a3a784392f276955900
안유진 <ujahnn@gmail.com>
2019-09-10 15:08:19 +0900

    Update WebClient.ino to Repeat

--- WebClient/WebClient.ino
+++ WebClient/WebClient.ino
@@ -1,10 +1,11 @@
 /*
- WiFiEsp example: WebClient
+ WiFiEsp example: WebClientRepeating
 
- This sketch connects to google website using an ESP8266 module to
- perform a simple web search.
+ This sketch connects to a web server and makes an HTTP request
+ using an Arduino ESP8266 module.
+ It repeats the HTTP call each 10 seconds.
 
- For more details see: http://yaab-arduino.blogspot.com/p/wifiesp-example-client.html
+ For more details see: http://yaab-arduino.blogspot.com/p/wifiesp.html
 */
 
 #include "WiFiEsp.h"
@@ -13,19 +14,18 @@
 // Emulate Serial1 on pins 6/7 if not present
 #ifndef HAVE_HWSERIAL1
 #include "SoftwareSerial.h"
-
 SoftwareSerial Serial1(2, 3); // RX, TX
 SoftwareSerial mySerial(5, 11); //RX, TX
-
 #endif
 
 char ssid[] = "pleasebreath";            // your network SSID (name)
 char pass[] = "tnawhatnlwk1";        // your network password
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
-int port = 3000;
 
-char server[] = "192.168.0.9";
+char server[] = "192.168.0.2";
 
+unsigned long lastConnectionTime = 0;         // last time you connected to the server, in milliseconds
+const unsigned long postingInterval = 10000L; // delay between updates, in milliseconds
 
 HDC1080 hdcSensor;
 float tc, tf, h;
@@ -36,7 +36,7 @@ WiFiEspClient client;
 
 void setup()
 {
-  Wire.begin();
+ Wire.begin();
   hdcSensor.turnOnHeater(true);
   hdcSensor.setTemperatureRes(HDC1080::T_RES_14);
   hdcSensor.setHumidityRes(HDC1080::H_RES_8);
@@ -67,135 +67,99 @@ void setup()
     status = WiFi.begin(ssid, pass);
   }
 
-  // you're connected now, so print out the data
   Serial.println("You're connected to the network");
   
   printWifiStatus();
-
-  Serial.println();
-  Serial.println("Starting connection to server...");
-  // if you get a connection, report back via serial
-  
-  if (client.connect(server, port)) {
-    Serial.println("Connected to server");
-    // Make a HTTP request
-    //client.println("GET /asciilogo.txt HTTP/1.1");
-    client.println("GET http://192.168.0.9:3000/insertdb?tempOuter=777");
-    //client.println("Host: arduino.cc");
-    //client.println("Connection: close");
-    client.println();
-  }
 }
 
 void loop()
 {
-  // if there are incoming bytes available
-  // from the server, read them and print them
-  /*while (client.available()) {
+  // if there's incoming data from the net connection send it out the serial port
+  // this is for debugging purposes only
+
+  /*
+  while (client.available()) {
     char c = client.read();
     Serial.write(c);
   }*/
 
+  // if 10 seconds have passed since your last connection,
+  // then connect again and send data
+
+  
   mySerial.listen();
+  
+  if (millis() - lastConnectionTime > postingInterval) {
   if(mySerial.available()>=32){
      for(int j=0; j<32 ; j++){ 
       pms[j]=mySerial.read(); 
     } 
-     
-    int PM1_0=(pms[10]<<8)  | pms[11]; 
+
+     int PM1_0=(pms[10]<<8)  | pms[11]; 
     int PM2_5=(pms[12]<<8)  | pms[13];
     int PM10 =(pms[14]<<8)  | pms[15];
-    
+    /*
     Serial.print("PM1.0 : "); 
     Serial.print(PM1_0);
     Serial.print(" PM2.5 : "); 
     Serial.print(PM2_5);
     Serial.print(" PM1.0 : "); 
     Serial.println(PM10);
+    */
     tf = hdcSensor.getTemperatureHumidity(tc, h);
   
-  
-    Serial.print(tc);
-    Serial.print("C ");
-    Serial.print(h);
-    Serial.println("H");  
-
-    
-    Serial1.listen();
-      /*while ( status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
-    
-    // Connect to WPA/WPA2 network
-    status = WiFi.begin(ssid, pass);
-  }*/
-  
-    String str = "GET http://192.168.0.9:3000/insertdb?tempOuter=";
-    str+=tc;
-    str+="humidOuter=";
-    str+=h;
-    str+="pm10Outer=";
-    str+=PM10;
-    str+="pm25Outer=";
-    str+=PM2_5;
-    
-    
-    client.println(str);
-    client.println();
-    delay(1000);
-  }
-  
-     /* if(client.connected()){
-        
-        if(mySerial.available() >= 32){
-         for(int j=0; j<32 ; j++){ 
-            pms[j]=mySerial.read(); 
-         } 
-        }
-        
-        int PM1_0=(pms[10]<<8)  | pms[11]; 
-        int PM2_5=(pms[12]<<8)  | pms[13];
-        int PM10 =(pms[14]<<8)  | pms[15];
-        
-        Serial.print("PM1.0 : "); 
-        Serial.print(PM1_0);
-        Serial.print(" PM2.5 : "); 
-        Serial.print(PM2_5);
-        Serial.print(" PM1.0 : "); 
-        Serial.println(PM10);
-        tf = hdcSensor.getTemperatureHumidity(tc, h);
-      
-      
+  /*
     Serial.print(tc);
     Serial.print("C ");
     Serial.print(h);
     Serial.println("H");
+    */
 
-    String str = "GET http://192.168.0.9:3000/insertdb?tempOuter=";
+    Serial1.listen();
+    
+  String str = "GET http://192.168.0.2:3000?tempInner=";
     str+=tc;
-    str+="humidOuter=";
+    str+="&humidInner=";
     str+=h;
-    str+="pm10Outer=";
+    str+="&pm10Inner=";
     str+=PM10;
-    str+="pm25Outer=";
+    str+="&pm25Inner=";
     str+=PM2_5;
     
-    client.println(str);
-    client.println();
-    
-}
-  
-  
-  // if the server's disconnected, stop the client
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("Disconnecting from server...");
-    client.stop();
-
-    // do nothing forevermore
-    while (true);
+    httpRequest(str);
   }
-  */
+  }
+}
+
+// this method makes a HTTP connection to the server
+void httpRequest(String str)
+{ 
+    
+  Serial.println();
+    
+  // close any connection before send a new request
+  // this will free the socket on the WiFi shield
+  client.stop();
+  // if there's a successful connection
+  if (client.connect(server, 3000)) {
+    Serial.println("Connecting...");
+    
+    // send the HTTP PUT request
+    
+    client.println(str);
+    Serial.println(str);
+    //client.println(F("GET ?tempInner=777 HTTP/1.1"));
+    //client.println(F("Host: arduino.cc"));
+    //client.println("Connection: close");
+    client.println();
+
+    // note the time that the connection was made
+    lastConnectionTime = millis();
+  }
+  else {
+    // if you couldn't make a connection
+    Serial.println("Connection failed");
+  }
 }
 
 
