cf2ac6b3468093936e9bf38ed8e75ee2e07e8b5e
조현학 <zhh102000@gmail.com>
2019-10-17 20:46:00 +0900

    [RUN-95][Open() 180도 회전, Close() 반대로 180도 회전][모터개발]

---
+++ Motor/Motor.ino
@@ -0,0 +1,45 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+bool is_open = false;
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+
+
+void loop() {
+  if(!is_open){
+    is_open = true;
+    Open();
+  }
+  //Close();
+}
+
+void Open(){
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
+
+void Close(){
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
