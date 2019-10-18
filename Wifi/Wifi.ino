286c83961ad726ec4c4cbf2c7887b53996ef6cfe
조현학 <zhh102000@gmail.com>
2019-10-18 15:29:40 +0900

    [RUN-95][/O = open C = close 기능 풀네임으로 명령을 보내려고 했으나 왜인지 모를 오류 발생][모터기능 개발]

--- Wifi/Wifi.ino
+++ Wifi/Wifi.ino
@@ -4,7 +4,7 @@ SoftwareSerial mySerial(2, 3); // RX, TX
 
 void setup() {
   Serial.begin(9600);
-  mySerial.begin(115200);//기본 통신 속도가 9600인 제품은 9600으로 수정해 주세요
+  mySerial.begin(9600);//기본 통신 속도가 9600인 제품은 9600으로 수정해 주세요
 }
 
 void loop() {
